51c61a9a30d94e8409a7fbb9cc0d57b61dd88d42
Pranav <96674501+prgit21@users.noreply.github.com>
2022-10-10 22:29:36 +0530

    fixed range
    istg idk how it makes a difference

--- running-sum.py
+++ running-sum.py
@@ -1,7 +1,5 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        i =1
-        for i in range (n):
+        for i in range (1,len(nums)):
             nums[i]+=nums[i-1]
         return nums
